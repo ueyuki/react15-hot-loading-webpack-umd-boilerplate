99c0f6fdbd0421bbf9cabe1410e33fbb5059ef44
ueyuki <touroku146749@gmail.com>
2017-05-26 17:45:01 +0900

    Add Markdown

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import Remarkable from 'remarkable'
 
 export default class CommentForm extends React.Component {
+  rawMarkup() {
+    const md = new Remarkable();
+    const rawMarkup = md.render(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup};
+  }
+
   render() {
     return (
         <div className="comment">
           <h2 className="commentAuthor">
             {this.props.author}
           </h2>
-          {this.props.children}
+          <span dangerouslySetInnerHTML={this.rawMarkup()} />
         </div>
-    )
+    );
   }
 }
